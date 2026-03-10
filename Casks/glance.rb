403ae9954ef82199b0406d4e94c39bda59379489
Azim Sukhanov <azimsukhanov@Mac-mini-Azim.local>
2026-03-10 18:53:29 +0300

    chore: bump Glance cask to v1.1.2

--- Casks/glance.rb
+++ Casks/glance.rb
@@ -1,6 +1,6 @@
 cask "glance" do
-  version "1.1.1"
-  sha256 "f3a5b454d95bc06ff899df7bd7b39cb2fb9a1780ea42816b7006d69d85a2ea21"
+  version "1.1.2"
+  sha256 "79b2daaa287b0e3229450d630e1a62b89d6775b06b2ef2fb1fa5adad43a0846e"
 
   url "https://github.com/azixxxxx/glance/releases/download/v#{version}/Glance-#{version}.zip"
   name "Glance"
